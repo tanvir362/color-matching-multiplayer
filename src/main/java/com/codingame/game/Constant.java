94841edd3679e8a3776c599575c68f60a81aca84
Tanvir Ahmed <tanvir66288@gmai.com>
2022-09-04 02:38:37 +0600

    TA | declared CARD_TYPES as List

--- src/main/java/com/codingame/game/Constant.java
+++ src/main/java/com/codingame/game/Constant.java
@@ -1,9 +1,12 @@
 package com.codingame.game;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class Constant {
 
-    public static final String[] CARD_TYPES = {"A", "B", "C", "D"};
+    public static final List<String> CARD_TYPES = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
     public static final int CARDS_PER_PLAYER = 4;
 }
